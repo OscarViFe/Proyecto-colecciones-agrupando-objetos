3fac7bf543d5cfb4eb0d76f7bf4bc940716ea12e
Marckiuss <cram.ferrero@gmail.com>
2019-02-06 13:57:09 +0100

    Commit 04: Añadido el método getCaracterísticas el cual nos devuelve las características del televisor deseado

--- Television.java
+++ Television.java
@@ -15,7 +15,7 @@ public class Television
     private boolean fhd;
 
     private LocalDate fechaCompra;
-    
+
     private int numeroIdentificacion;
 
     /**
@@ -30,37 +30,82 @@ public class Television
         this.fechaCompra = fechaCompra;
         fechaCompra = LocalDate.parse(fechaComprado);
         this.numeroIdentificacion = numeroIdentificacion;
+        String stringFHD = "";
     }
 
+    /**
+     * devuelve el numero de pulgadas del televisor
+     */
     public int getPulgadas(){
         return pulgadas;
     }
-
+    
+    /**
+     * establece el numero de pulgadas del televisor
+     */
     public void setPulgadas(int nuevasPulgadas){
         pulgadas = nuevasPulgadas;
     }
-
+    
+    /**
+     * devuelve la marca del televisor
+     */
     public String getMarca(){
         return marca;
     }
-
+    
+    /**
+     * establece la marca del televisor
+     */
     public void setMarca(String nuevaMarca){
         marca = nuevaMarca;
     }
-
+    
+    /**
+     * devuelve true si el televisor es full hd
+     */
     public boolean GetFHD(){
         return fhd;
     }
-
+    
+    /**
+     * establece que un televisor es full hd
+     */
     public void setFHD(){
         fhd = true;
     }
-
+    
+    /**
+     * devuelve la fecha en la que se compró el televisor
+     */
     public LocalDate getFechaCompra(){
         return fechaCompra;
     }
-
+    
+    /**
+     * establece la fecha en la que se compró el televisor
+     */
     public void setFechaCompra(int dia, int mes, int ano){
         fechaCompra = LocalDate.of(ano, mes, dia);
+    }
+    
+    /**
+     * convierte el boolean Full HD en una cadena de caracteres que nos dicen en lenguaje hablado silo es o no
+     */
+    public String stringFHD(){
+        String stringFHD = "";
+        if (fhd){
+            stringFHD = "4k Full HD";
+        }
+        else{
+            stringFHD = "1080p";
+        }
+        return stringFHD;
+    }
+
+    public String getCaracterísticas(){
+        String caracteristicas = "";
+        caracteristicas = "Television " + marca + " " + pulgadas + " pulgadas " + stringFHD() + " " + fechaCompra + " " + "identificador: " + numeroIdentificacion + "";
+        return caracteristicas;
     }
 }
